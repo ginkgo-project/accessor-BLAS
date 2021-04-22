b5b71be45c34177c2bdbc617e8f5f62b724c2964
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2021-04-22 19:17:09 +0200

    Increase matrix size continuously

--- cuda/main.cu
+++ cuda/main.cu
@@ -1,12 +1,12 @@
 #include <array>
+#include <cmath>
 #include <ios>
 #include <iostream>
+#include <limits>
 #include <random>
 #include <stdexcept>
 #include <type_traits>
 #include <vector>
-#include <limits>
-#include <cmath>
 
 #include "kernels.cuh"
 #include "utils.cuh"
@@ -31,8 +31,10 @@ void compare(const matrix_info info, const std::vector<ValueType> &mtx1,
             const std::size_t midx = row * info.stride + col;
             const auto v1 = mtx1[midx];
             const auto v2 = mtx2[midx];
-            if (std::abs(v1 - v2) > std::numeric_limits<ValueType>::epsilon() * 8) {
-                std::cout << "Mismatch at (" << row << ", " << col << "): " << v1 << " vs. " << v2 << '\n';
+            if (std::abs(v1 - v2) >
+                std::numeric_limits<ValueType>::epsilon() * 8) {
+                std::cout << "Mismatch at (" << row << ", " << col
+                          << "): " << v1 << " vs. " << v2 << '\n';
             }
         }
     }
@@ -47,107 +49,122 @@ void test(t1, t2, t3, t4, t5) {
 int main() {
     using vtype = double;
     using stype = float;
-    constexpr int bench_iters {10};
-    constexpr std::int32_t num_rows{16 * 1024};
-    constexpr matrix_info minfo{{num_rows, num_rows}};
-    
-    std::cout << "Total matrix size: " << minfo.get_1d_size() * sizeof(vtype) << '\n';
-    //static_assert(minfo.size[0] == minfo.size[1], "Matrix must be square!");
-    
+    constexpr int bench_iters{10};
+    constexpr std::int32_t max_rows{24 * 1024};
+    constexpr std::int32_t max_mtx_size{max_rows * max_rows};
     std::default_random_engine rengine(42);
     std::uniform_real_distribution<stype> val_dist(1.0, 2.0);
     std::uniform_real_distribution<stype> one_dist(1.0, 1.0);
+    constexpr matrix_info max_minfo{{max_rows, max_rows}};
+    constexpr char DELIM{';'};
 
-    const matrix_info vinfo{{minfo.size[0], 1}};
+    const matrix_info max_vinfo{{max_rows, 1}};
 
-    auto v_matrix = gen_mtx<vtype>(minfo, val_dist, rengine);
-    auto s_matrix = convert_mtx<stype>(minfo, v_matrix);
+    auto v_matrix = gen_mtx<vtype>(max_minfo, val_dist, rengine);
+    auto s_matrix = convert_mtx<stype>(max_minfo, v_matrix);
 
-    auto v_b = gen_mtx<vtype>(vinfo, one_dist, rengine);
-    auto s_b = convert_mtx<stype>(vinfo, v_matrix);
+    auto v_b = gen_mtx<vtype>(max_vinfo, one_dist, rengine);
+    auto s_b = convert_mtx<stype>(max_vinfo, v_matrix);
 
-    auto v_res = std::vector<vtype>(vinfo.get_1d_size(), vtype{});
-    auto s_res = std::vector<stype>(vinfo.get_1d_size(), stype{});
+    auto v_res = std::vector<vtype>(max_vinfo.get_1d_size(), vtype{});
+    auto s_res = std::vector<stype>(max_vinfo.get_1d_size(), stype{});
 
-    auto dv_matrix = GpuMemory<vtype>(minfo.get_1d_size());
+    auto dv_matrix = GpuMemory<vtype>(max_minfo.get_1d_size());
     dv_matrix.copy_from(v_matrix);
-    auto ds_matrix = GpuMemory<stype>(minfo.get_1d_size());
+    auto ds_matrix = GpuMemory<stype>(max_minfo.get_1d_size());
     ds_matrix.copy_from(s_matrix);
 
-    auto dv_b = GpuMemory<vtype>(vinfo.get_1d_size());
+    auto dv_b = GpuMemory<vtype>(max_vinfo.get_1d_size());
     dv_b.copy_from(v_b);
-    auto ds_b = GpuMemory<stype>(vinfo.get_1d_size());
+    auto ds_b = GpuMemory<stype>(max_vinfo.get_1d_size());
     ds_b.copy_from(s_b);
-    auto dv_res = GpuMemory<vtype>(vinfo.get_1d_size());
-    auto ds_res = GpuMemory<stype>(vinfo.get_1d_size());
+    auto dv_res = GpuMemory<vtype>(max_vinfo.get_1d_size());
+    auto ds_res = GpuMemory<stype>(max_vinfo.get_1d_size());
 
-    CudaTimer ctimer;
-    // Benchmark double
-    // Warmup
-    spmv(minfo, dv_matrix.data(), vinfo, dv_b.data(), dv_res.data());
-    synchronize();
-    
-    double double_time{};
-    for (int i = 0; i < bench_iters; ++i) {
-        ctimer.start();
+    std::cout << "Num Rows" << DELIM << "GEMV double" << DELIM << "GEMV float"
+        << DELIM << "GEMV Acc<fp64, fp64>"<< DELIM << "GEMV Acc<fp32, fp64>" << '\n';
+
+    constexpr int start{max_rows / 48};
+    constexpr int row_incr{start};
+    for (int num_rows = start; num_rows <= max_rows; num_rows += row_incr) {
+        std::cout << num_rows << DELIM;
+
+        const matrix_info minfo{{num_rows, num_rows}};
+        const matrix_info vinfo{{num_rows, 1}};
+
+        CudaTimer ctimer;
+
+        // static_assert(minfo.size[0] == minfo.size[1], "Matrix must be
+        // square!");
+
+        // Benchmark double
+        // Warmup
         spmv(minfo, dv_matrix.data(), vinfo, dv_b.data(), dv_res.data());
-        ctimer.stop();
-        double_time += ctimer.get_time();
-        ctimer.reset();
-    }
-    std::cout << "double time: " << double_time / bench_iters << " ms\n";
-    
-    // Benchmark float
-    // Warmup
-    spmv(minfo, ds_matrix.data(), vinfo, ds_b.data(), ds_res.data());
-    synchronize();
-    
-    double single_time{};
-    for (int i = 0; i < bench_iters; ++i) {
-        ctimer.start();
+        synchronize();
+
+        double double_time{};
+        for (int i = 0; i < bench_iters; ++i) {
+            ctimer.start();
+            spmv(minfo, dv_matrix.data(), vinfo, dv_b.data(), dv_res.data());
+            ctimer.stop();
+            double_time += ctimer.get_time();
+            ctimer.reset();
+        }
+        std::cout << double_time / bench_iters << DELIM;
+
+        // Benchmark float
+        // Warmup
         spmv(minfo, ds_matrix.data(), vinfo, ds_b.data(), ds_res.data());
-        ctimer.stop();
-        single_time += ctimer.get_time();
-        ctimer.reset();
+        synchronize();
+
+        double single_time{};
+        for (int i = 0; i < bench_iters; ++i) {
+            ctimer.start();
+            spmv(minfo, ds_matrix.data(), vinfo, ds_b.data(), ds_res.data());
+            ctimer.stop();
+            single_time += ctimer.get_time();
+            ctimer.reset();
+        }
+        std::cout << single_time / bench_iters << DELIM;
+
+        // Benchmark Accessor<fp64, fp32>
+        // Warmup
+        acc_spmv<vtype>(minfo, ds_matrix.data(), vinfo, ds_b.data(),
+                        ds_res.data());
+        synchronize();
+
+        double acc_dd_time{};
+        for (int i = 0; i < bench_iters; ++i) {
+            ctimer.start();
+            acc_spmv<vtype>(minfo, dv_matrix.data(), vinfo, dv_b.data(),
+                            dv_res.data());
+            ctimer.stop();
+            acc_dd_time += ctimer.get_time();
+            ctimer.reset();
+        }
+        std::cout << acc_dd_time / bench_iters << DELIM;
+
+        double acc_df_time{};
+        for (int i = 0; i < bench_iters; ++i) {
+            ctimer.start();
+            acc_spmv<vtype>(minfo, ds_matrix.data(), vinfo, ds_b.data(),
+                            ds_res.data());
+            ctimer.stop();
+            acc_df_time += ctimer.get_time();
+            ctimer.reset();
+        }
+        std::cout << acc_df_time / bench_iters << '\n';
+
     }
-    std::cout << "single time: " << single_time / bench_iters << " ms\n";
-    
-    // Benchmark Accessor<fp64, fp32>
-    // Warmup
-    acc_spmv<vtype>(minfo, ds_matrix.data(), vinfo, ds_b.data(), ds_res.data());
-    synchronize();
-    
-    double acc_time{};
-    for (int i = 0; i < bench_iters; ++i) {
-        ctimer.start();
-        acc_spmv<vtype>(minfo, ds_matrix.data(), vinfo, ds_b.data(), ds_res.data());
-        ctimer.stop();
-        acc_time += ctimer.get_time();
-        ctimer.reset();
-    }
-    std::cout << "Access time: " << acc_time / bench_iters << " ms\n";
-    
-    double acc2_time{};
-    for (int i = 0; i < bench_iters; ++i) {
-        ctimer.start();
-        acc_spmv<vtype>(minfo, dv_matrix.data(), vinfo, dv_b.data(), dv_res.data());
-        ctimer.stop();
-        acc2_time += ctimer.get_time();
-        ctimer.reset();
-    }
-    std::cout << "Acces2 time: " << acc2_time / bench_iters << " ms\n";
-    
+
     auto gpu_res = dv_res.get_vector();
 
     // Control impl:
-    control_spmv(minfo, v_matrix, vinfo, v_b, v_res);
+    control_spmv(max_minfo, v_matrix, max_vinfo, v_b, v_res);
 
     std::cout.precision(16);
     std::cout << std::scientific;
 
-    // print_mtx(vinfo, v_res);
     std::cout << "Comparison:\n";
-    // print_mtx(vinfo, gpu_res);
-    compare(vinfo, v_res, gpu_res);
-
+    compare(max_vinfo, v_res, gpu_res);
 }
