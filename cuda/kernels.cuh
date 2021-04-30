b5691daa8cbc7b8a040ba09bac496c96c5c1d72e
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2021-04-30 06:00:47 +0200

    Use different info parameter for result (own stride, size, ...)

--- cuda/kernels.cuh
+++ cuda/kernels.cuh
@@ -27,9 +27,9 @@ __device__ void reduce(Group &&group, ValueType *__restrict__ shared) {
 template <std::int64_t block_size, typename ValueType>
 __global__ __launch_bounds__(block_size) void gemv(
     const matrix_info minfo, ValueType alpha, const ValueType *__restrict__ mtx,
-    const matrix_info vinfo, const ValueType *__restrict__ x, ValueType beta,
+    const matrix_info x_info, const ValueType *__restrict__ x, const matrix_info res_info, ValueType beta,
     ValueType *__restrict__ res) {
-    // expect vinfo.size[1] == 1
+    // expect x_info.size[1] == 1
     const std::int64_t row_idx{blockIdx.x};
     if (row_idx > minfo.size[0]) {
         return;
@@ -39,24 +39,22 @@ __global__ __launch_bounds__(block_size) void gemv(
     __shared__ char shared_impl[sizeof(ValueType) * block_size];
     auto shared = reinterpret_cast<ValueType *>(shared_impl);
     ValueType local_result{};
-    const ValueType old_res =
-        (beta == 0) ? ValueType{0} : res[row_idx * vinfo.stride];
     const auto group = cg::this_thread_block();
     const auto local_id = group.thread_rank();
 
     for (std::int64_t col = local_id; col < minfo.size[1]; col += block_size) {
         const auto mtx_val = mtx[row_start + col];
-        const auto b_val = x[col * vinfo.stride];
+        const auto b_val = x[col * x_info.stride];
         local_result += mtx_val * b_val;
     }
     shared[local_id] = local_result;
     reduce(group, shared);
     if (local_id == 0) {
-        const auto v_idx = row_idx * vinfo.stride;
+        const auto res_idx = row_idx * res_info.stride;
         if (beta == ValueType{0}) {
-            res[v_idx] = alpha * shared[local_id];
+            res[res_idx] = alpha * shared[local_id];
         } else {
-            res[v_idx] = alpha * shared[local_id] + beta * res[v_idx];
+            res[res_idx] = alpha * shared[local_id] + beta * res[res_idx];
         }
     }
 }
@@ -71,7 +69,7 @@ __global__ __launch_bounds__(block_size) void acc_gemv(ArType alpha,
     static_assert(std::is_same<ArType, ar_type>::value, "Types must be equal!");
     // static_assert(std::is_same<ar_type, double>::value, "Type must be
     // double!!!");
-    // expect vinfo.size[1] == 1
+    // expect x_info.size[1] == 1
     const std::int64_t row_idx{blockIdx.x};
     if (row_idx > mtx.length(0)) {
         return;
@@ -107,7 +105,7 @@ ValueType ceildiv(ValueType a, ValueType b) {
 
 template <typename ValueType>
 void gemv(const matrix_info minfo, ValueType alpha, const ValueType *mtx,
-          const matrix_info vinfo, const ValueType *x, ValueType beta,
+          const matrix_info x_info, const ValueType *x, const matrix_info res_info, ValueType beta,
           ValueType *res) {
     constexpr std::int32_t block_size{512};
     const dim3 block(block_size, 1, 1);
@@ -115,12 +113,12 @@ void gemv(const matrix_info minfo, ValueType alpha, const ValueType *mtx,
     const dim3 grid(minfo.size[0], 1, 1);
 
     kernel::gemv<block_size, ValueType>
-        <<<grid, block>>>(minfo, alpha, mtx, vinfo, x, beta, res);
+        <<<grid, block>>>(minfo, alpha, mtx, x_info, x, res_info, beta, res);
 }
 
 template <typename ArType, typename StType>
 void acc_gemv(const matrix_info minfo, ArType alpha, const StType *mtx,
-              const matrix_info vinfo, const StType *x, ArType beta,
+              const matrix_info x_info, const StType *x, const matrix_info res_info, ArType beta,
               StType *res) {
     constexpr std::int32_t block_size{512};
     const dim3 block(block_size, 1, 1);
@@ -130,18 +128,19 @@ void acc_gemv(const matrix_info minfo, ArType alpha, const StType *mtx,
     // Accessor Setup
     constexpr std::size_t dimensionality{2};
     std::array<std::size_t, dimensionality - 1> m_stride{minfo.stride};
-    std::array<std::size_t, dimensionality - 1> v_stride{vinfo.stride};
+    std::array<std::size_t, dimensionality - 1> x_stride{x_info.stride};
+    std::array<std::size_t, dimensionality - 1> res_stride{res_info.stride};
 
     using accessor =
         gko::acc::reduced_row_major<dimensionality, ArType, StType>;
     using range = gko::acc::range<accessor>;
     using c_range = gko::acc::range<typename accessor::const_accessor>;
     auto m_acc = c_range(minfo.size, mtx, m_stride);
-    auto b_acc = c_range(vinfo.size, x, v_stride);
-    auto res_acc = range(vinfo.size, res, v_stride);
+    auto x_acc = c_range(x_info.size, x, x_stride);
+    auto res_acc = range(res_info.size, res, res_stride);
 
     kernel::acc_gemv<block_size>
-        <<<grid, block>>>(alpha, m_acc, b_acc, beta, res_acc);
+        <<<grid, block>>>(alpha, m_acc, x_acc, beta, res_acc);
 }
 
 #define BIND_CUBLAS_GEMM(ValueType, CublasName)                                \
@@ -171,8 +170,8 @@ BIND_CUBLAS_GEMV(float, cublasSgemv)
 
 template <typename ValueType>
 void cublas_gemv(cublasHandle_t handle, const matrix_info minfo,
-                 ValueType alpha, const ValueType *mtx, const matrix_info vinfo,
-                 const ValueType *x, ValueType beta, ValueType *y) {
+                 ValueType alpha, const ValueType *mtx, const matrix_info x_info,
+                 const ValueType *x, const matrix_info res_info, ValueType beta, ValueType *y) {
     // Note: CUBLAS expects the matrices to be stored in column major,
     //       so the sizes will be transposed for the cublas call
     // auto alpha = ValueType{1};
@@ -181,14 +180,14 @@ void cublas_gemv(cublasHandle_t handle, const matrix_info minfo,
     cublas_gemv(handle, CUBLAS_OP_T, static_cast<int>(minfo.size[0]),
                 static_cast<int>(minfo.size[1]), &alpha, mtx,
                 static_cast<int>(minfo.stride), x,
-                static_cast<int>(vinfo.stride), &beta, y,
-                static_cast<int>(vinfo.stride));
+                static_cast<int>(x_info.stride), &beta, y,
+                static_cast<int>(res_info.stride));
     /*/
     cublas_gemm(
-        handle, CUBLAS_OP_N, CUBLAS_OP_N, static_cast<int>(vinfo.size[1]),
-        static_cast<int>(vinfo.size[0]), static_cast<int>(minfo.size[1]),
-        &alpha, x, vinfo.stride, mtx, static_cast<int>(minfo.stride), &beta,
-        y, static_cast<int>(vinfo.stride));
+        handle, CUBLAS_OP_N, CUBLAS_OP_N, static_cast<int>(x_info.size[1]),
+        static_cast<int>(x_info.size[0]), static_cast<int>(minfo.size[1]),
+        &alpha, x, x_info.stride, mtx, static_cast<int>(minfo.stride), &beta,
+        y, static_cast<int>(res_info.stride));
     //*/
 }
 
