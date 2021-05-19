012baf5cd17d7e5620f62bf1a5f8b32236194806
Thomas Grützmacher <thomas.gruetzmacher@kit.edu>
2021-05-19 02:24:37 +0200

    Add first version of working TRSV

---
+++ cuda/trsv_benchmark.cu
@@ -0,0 +1,251 @@
+#include <array>
+#include <cmath>
+#include <ios>
+#include <iostream>
+#include <limits>
+#include <random>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <type_traits>
+
+//#include "../error_tobias.hpp"
+#include "memory.cuh"
+#include "trsv_kernels.cuh"
+#include "trsv_memory.cuh"
+#include "utils.cuh"
+
+template <typename OutputType, typename InputType, typename ReduceOp>
+OutputType reduce(const matrix_info info, InputType *tmp, ReduceOp op) {
+    std::size_t end = info.size[0];
+    for (std::size_t halfway = ceildiv(info.size[0], std::size_t{2});
+         halfway > 1; halfway = ceildiv(halfway, std::size_t{2})) {
+        for (std::size_t row = 0; row < halfway; ++row) {
+            if (row + halfway < end) {
+                for (std::size_t col = 0; col < info.size[1]; ++col) {
+                    const std::size_t midx = row * info.stride + col;
+                    const std::size_t midx2 =
+                        (row + halfway) * info.stride + col;
+                    tmp[midx] = op(tmp[midx], tmp[midx2]);
+                }
+            }
+        }
+        end = halfway;
+    }
+    return static_cast<OutputType>(info.size[0] == 1 ? op(tmp[0], {})
+                                                     : op(tmp[0], tmp[1]));
+}
+
+template <typename T>
+std::enable_if_t<std::is_floating_point<T>::value, T> get_value(T val) {
+    return val;
+}
+
+template <typename T>
+std::enable_if_t<!std::is_floating_point<T>::value, typename T::value_type>
+get_value(T val) {
+    return val.e;
+}
+
+template <typename ReferenceType, typename OtherType, typename ValueType>
+ValueType compare(const matrix_info info, const ReferenceType *mtx1,
+                  const OtherType *mtx2, ValueType *tmp) {
+    using return_type = decltype(get_value(ReferenceType{}));
+    static_assert(std::is_same<return_type, ValueType>::value,
+                  "Types must match!");
+
+    for (std::size_t row = 0; row < info.size[0]; ++row) {
+        const std::size_t midx = row * info.stride;
+        const auto v1 = get_value(mtx1[midx]);
+        const decltype(v1) v2 = get_value(mtx2[midx]);
+        if (std::is_floating_point<decltype(v1)>::value) {
+            const auto delta = std::abs(v1 - v2);
+            tmp[midx] = delta;
+        } else {
+            // only compute the 1-norm of the error
+            const auto error = std::abs(v2);
+            tmp[midx] = error;
+        }
+    }
+
+    return reduce<ValueType>(
+        info, tmp, [](ValueType o1, ValueType o2) { return o1 + o2; });
+}
+
+int main(int argc, char **argv) {
+    /*
+    using ar_type = error_number<double>;
+    using st_type = error_number<float>;
+    using value_type = ar_type::value_type;
+    /*/
+    using ar_type = double;
+    using st_type = float;
+    using value_type = ar_type;
+    //*/
+
+    constexpr tmtx_t t_matrix_type = tmtx_t::lower;
+    constexpr dmtx_t d_matrix_type = dmtx_t::unit;
+
+    bool measure_error{false};
+
+    const std::string use_error_string("--error");
+    if (argc == 2 && std::string(argv[1]) == use_error_string) {
+        measure_error = true;
+    } else if (argc > 1) {
+        const std::string binary(argv[0]);
+        std::cerr << "Unsupported parameters!\n";
+        std::cerr << "Usage: " << binary << " [" << use_error_string << "]\n";
+        std::cerr << "With " << use_error_string
+                  << ":    compute error of GeMVs\n"
+                  << "Without parameters: benchmark different GeMVs\n";
+        return 1;
+    }
+
+    // constexpr std::size_t max_rows{24 * 1024};
+    constexpr std::size_t max_rows{5000};
+    // constexpr std::size_t max_rows{8 * 1024};
+    constexpr std::size_t max_cols{max_rows};
+    constexpr char DELIM{';'};
+    
+    constexpr std::size_t start = max_rows - 10;//std::max(std::size_t{32}, max_rows / 50);  // max_rows / 48;
+    // constexpr auto start = max_rows / 48;
+    constexpr std::size_t row_incr = 1; // start;
+
+    std::default_random_engine rengine(42);
+    std::uniform_real_distribution<value_type> mtx_dist(0.0, 1.0);
+    // std::uniform_real_distribution<value_type> mtx_dist(-10.0, 10.0);
+    auto vector_dist = mtx_dist;
+
+    auto ar_data =
+        TrsvMemory<ar_type>(max_rows, max_cols, mtx_dist, vector_dist, rengine);
+    auto st_data = TrsvMemory<st_type>(ar_data);
+
+    auto cublasHandle = cublas_get_handle();
+
+    static_assert(max_rows == max_cols, "Matrix must be square!");
+
+    // Additional memory and lambdas for error computation
+    const auto max_res_num_elems = ar_data.cpu_x_memory().get_num_elems();
+    Memory<ar_type> cpu_x_ref(Memory<ar_type>::Device::cpu, max_res_num_elems);
+    value_type res_ref_norm{1.0};
+    Memory<value_type> reduce_memory(Memory<ar_type>::Device::cpu,
+                                     max_res_num_elems);
+    auto ar_compute_error = [&](matrix_info x_info) {
+        value_type error{};
+        ar_data.sync_x();
+        error = compare(x_info, cpu_x_ref.const_data(), ar_data.cpu_x_const(),
+                        reduce_memory.data());
+        //std::cout << '\n';
+        //print_mtx(x_info, ar_data.cpu_x_const());
+        ar_data.reset_x();
+        return error / res_ref_norm;
+    };
+    auto st_compute_error = [&](matrix_info x_info) {
+        value_type error{};
+        st_data.sync_x();
+        error = compare(x_info, cpu_x_ref.const_data(), st_data.cpu_x_const(),
+                        reduce_memory.data());
+        st_data.reset_x();
+        return error / res_ref_norm;
+    };
+
+    // Setting up names and associated benchmark and error functions
+
+    constexpr std::size_t benchmark_num{4};
+    constexpr std::size_t benchmark_reference{3};
+    using benchmark_info_t =
+        std::tuple<std::string, std::function<void(matrix_info, matrix_info)>,
+                   std::string, std::function<value_type(matrix_info)>>;
+    std::array<benchmark_info_t, benchmark_num> benchmark_info = {
+        benchmark_info_t{"TRSV fp64",
+                         [&](matrix_info m_info, matrix_info x_info) {
+                             trsv(m_info, t_matrix_type, d_matrix_type,
+                                  ar_data.gpu_mtx_const(), x_info,
+                                  ar_data.gpu_x());
+                         },
+                         "Error TRSV fp64", ar_compute_error},
+        benchmark_info_t{"CUBLAS TRSV fp64",
+                         [&](matrix_info m_info, matrix_info x_info) {
+                             cublas_trsv(cublasHandle.get(), t_matrix_type,
+                                         d_matrix_type, m_info,
+                                         ar_data.gpu_mtx_const(), x_info,
+                                         ar_data.gpu_x());
+                         },
+                         "Error CUBLAS TRSV fp64", ar_compute_error},
+        benchmark_info_t{"CUBLAS TRSV fp32",
+                         [&](matrix_info m_info, matrix_info x_info) {
+                             cublas_trsv(cublasHandle.get(), t_matrix_type,
+                                         d_matrix_type, m_info,
+                                         st_data.gpu_mtx_const(), x_info,
+                                         st_data.gpu_x());
+                         },
+                         "Error CUBLAS TRSV fp32", st_compute_error},
+        benchmark_info_t{"Hand TRSV fp64",
+                         [&](matrix_info m_info, matrix_info x_info) {
+                             control_trsv(m_info, t_matrix_type, d_matrix_type,
+                                          ar_data.cpu_mtx_const(), x_info,
+                                          ar_data.cpu_x());
+                         },
+                         "Error Hand TRSV fp64",
+                         [&](matrix_info x_info) {
+                             value_type error{};
+                             if (measure_error) {
+                                 error = compare(x_info, cpu_x_ref.const_data(),
+                                                 ar_data.cpu_x_const(),
+                                                 reduce_memory.data());
+                             }
+                             ar_data.reset_x();
+                             return error / res_ref_norm;
+                         }},
+    };
+
+    std::cout << "Num rows";
+    for (const auto &info : benchmark_info) {
+        if (!measure_error) {
+            std::cout << DELIM << std::get<0>(info);
+        } else {
+            std::cout << DELIM << std::get<2>(info);
+        }
+    }
+    std::cout << '\n';
+
+    std::cout.precision(16);
+    std::cout << std::scientific;
+
+    // std::vector<std::array<value_type, benchmark_num>>
+
+    for (auto num_rows = start; num_rows <= max_rows; num_rows += row_incr) {
+        std::array<value_type, benchmark_num> local_res{};
+        const matrix_info m_info{{num_rows, num_rows}, num_rows};
+        const matrix_info x_info{{num_rows, 1}};
+
+        if (measure_error) {
+            std::get<1>(benchmark_info[benchmark_reference])(m_info, x_info);
+            cpu_x_ref = ar_data.cpu_x_memory();
+            res_ref_norm = reduce<value_type>(
+                x_info, cpu_x_ref.data(),
+                [](ar_type a, ar_type b) { return std::abs(a) + std::abs(b); });
+            // copy again since reduce overwrites
+            cpu_x_ref = ar_data.cpu_x_memory();
+            //*/
+            ar_data.reset_x();
+        }
+        for (std::size_t i = 0; i < benchmark_num; ++i) {
+            auto local_func = [&]() {
+                std::get<1>(benchmark_info[i])(m_info, x_info);
+            };
+            if (!measure_error) {
+                local_res[i] = benchmark_function(local_func, measure_error);
+            } else {
+                benchmark_function(local_func, measure_error);
+                local_res[i] = std::get<3>(benchmark_info[i])(x_info);
+            }
+        }
+
+        std::cout << num_rows;
+        for (const auto &res : local_res) {
+            std::cout << DELIM << res;
+        }
+        std::cout << '\n';
+    }
+}
---
+++ cuda/trsv_kernels.cuh
@@ -0,0 +1,285 @@
+#pragma once
+
+#include <cooperative_groups.h>
+#include <cublas_v2.h>
+
+#include <accessor/range.hpp>
+#include <accessor/reduced_row_major.hpp>
+#include <cinttypes>
+
+#include "kernel_utils.cuh"
+#include "utils.cuh"
+
+enum class tmtx_t { upper, lower };
+enum class dmtx_t { non_unit, unit };
+
+namespace kernel {
+
+// Must be called with 2-D blocks, and 1-D grid
+template <std::int64_t block_size>
+__global__ __launch_bounds__(block_size) void test_idx() {
+    const std::int64_t diag_block_id = blockIdx.x;
+    const std::int64_t row_idx =
+        diag_block_id * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
+    const auto group = cg::this_thread_block();
+    const int idx = group.thread_rank();
+    const auto warp = cg::tiled_partition<WARP_SIZE>(group);
+    const int warp_idx = warp.thread_rank();
+
+    const int tidx = threadIdx.x;
+    const int tidy = threadIdx.y;
+
+    printf("%d, %d: id = %d; warp: %d\n", tidx, tidy, idx, warp_idx);
+}
+// Must be called with 2-D blocks, and a 1-element grid
+template <std::int64_t diag_block_size, dmtx_t dmtx, typename ValueType>
+__global__ __launch_bounds__(diag_block_size *diag_block_size) void lower_trsv(
+    const std::int64_t diag_block_start, const matrix_info m_info,
+    const ValueType *__restrict__ mtx, const matrix_info x_info,
+    ValueType *__restrict__ x) {
+    // assert: blockDim.x == blockDim.y == diag_block_size
+    // assert: blockIdx.x == blockIdx.y == 1
+    const std::int64_t row_idx = diag_block_start + threadIdx.x;
+    const std::int64_t col_idx = diag_block_start + threadIdx.y;
+    if (diag_block_start >= m_info.size[0]) {
+        return;
+    }
+    const auto group = cg::this_thread_block();
+    constexpr int shared_stride = diag_block_size + 1;
+    __shared__ ValueType shared[diag_block_size * shared_stride];
+
+    // Read into shared memory all necessary matrix data and store it transposed
+    // (with stride = BS+1)
+    if (threadIdx.x <= threadIdx.y && row_idx < m_info.size[0] &&
+        col_idx < m_info.size[1]) {
+        shared[threadIdx.x * shared_stride + threadIdx.y] =
+            (dmtx == dmtx_t::unit && threadIdx.x == threadIdx.y)
+                ? ValueType{1}
+                : mtx[row_idx + col_idx * m_info.stride];
+    }
+    group.sync();
+    auto warp = cg::tiled_partition<WARP_SIZE>(group);
+    if (group.thread_rank() / WARP_SIZE == 0) {
+        const auto warp_id = warp.thread_rank();
+        const std::int64_t x_idx = (diag_block_start + warp_id) * x_info.stride;
+        auto local_solution = x[x_idx];
+
+        for (int i = 0; i < WARP_SIZE; ++i) {
+            const auto mtx_val = shared[i * shared_stride + warp_id];
+            const auto current_x = warp.shfl(local_solution / mtx_val, i);
+            if (warp_id == i) {
+                local_solution = current_x;
+            }
+            if (warp_id > i) {
+                local_solution -= mtx_val * current_x;
+            }
+        }
+        // Write solution back
+        if (diag_block_start + warp_id < x_info.size[0]) {
+            x[x_idx] = local_solution;
+        }
+    }
+}
+
+// Must be called with 1-D blocks, and 1-D grid
+template <std::int64_t block_size, std::int64_t solved_size, typename ValueType>
+__global__ __launch_bounds__(block_size) void lower_trsv_apply(
+    const std::int64_t apply_start, const matrix_info m_info,
+    const ValueType *__restrict__ mtx, const matrix_info x_info,
+    ValueType *__restrict__ x) {
+    // assert: blockDim.x == blockDim.y == diag_block_size
+    // assert: blockDim.x % solved_size == 0
+    const std::int64_t global_id = blockIdx.x * blockDim.x + threadIdx.x;
+    const std::int64_t mtx_row =
+        apply_start + solved_size + global_id / solved_size;
+    const std::int64_t mtx_col = apply_start + global_id % solved_size;
+    if (mtx_row >= m_info.size[0]) {
+        return;
+    }
+    const auto group = cg::this_thread_block();
+    auto warp = cg::tiled_partition<WARP_SIZE>(group);
+    __shared__ ValueType cached_solution[solved_size];
+
+    // Fill in the cached solution with the first solved_size threads
+    if (group.thread_rank() < solved_size) {
+        cached_solution[group.thread_rank()] =
+            x[(apply_start + group.thread_rank()) * x_info.stride];
+    }
+    group.sync();
+    auto local_result =
+        (mtx_col < m_info.size[1])
+            ? mtx[mtx_row * m_info.stride + mtx_col] *
+                  cached_solution[warp.thread_rank() * x_info.stride]
+            : ValueType{0};
+    auto updated_x = reduce(warp, local_result,
+                            [](ValueType a, ValueType b) { return a + b; });
+    // each warp writes once
+    if (warp.thread_rank() == 0) {
+        x[mtx_row] -= updated_x;
+    }
+}
+
+template <std::int64_t block_size, typename MtxRange, typename XRange,
+          typename ResRange, typename ArType>
+__global__ __launch_bounds__(block_size) void acc_trsv(ArType alpha,
+                                                       MtxRange mtx, XRange x,
+                                                       ArType beta,
+                                                       ResRange res) {
+    using ar_type = decltype(mtx(0, 0) + mtx(0, 0));
+    static_assert(std::is_same<ArType, ar_type>::value, "Types must be equal!");
+    // static_assert(std::is_same<ar_type, double>::value, "Type must be
+    // double!!!");
+    // expect x_info.size[1] == 1
+    const std::int64_t row_idx{blockIdx.x};
+    if (row_idx >= mtx.length(0)) {
+        return;
+    }
+
+    const auto num_cols = mtx.length(1);
+    __shared__ char shared_impl[sizeof(ar_type) * block_size];
+    auto shared = reinterpret_cast<ar_type *>(shared_impl);
+    ar_type local_result{};
+    const auto group = cg::this_thread_block();
+    const auto local_id = group.thread_rank();
+
+    for (std::int64_t col = local_id; col < num_cols; col += block_size) {
+        local_result += mtx(row_idx, col) * x(col, 0);
+    }
+    shared[local_id] = local_result;
+    reduce(group, shared, [](ar_type a, ar_type b) { return a + b; });
+    if (local_id == 0) {
+        if (beta == ArType{0}) {
+            res(row_idx, 0) = alpha * shared[local_id];
+        } else {
+            res(row_idx, 0) = alpha * shared[local_id] + beta * res(row_idx, 0);
+        }
+    }
+}
+
+}  // namespace kernel
+
+template <typename ValueType>
+void control_trsv(const matrix_info m_info, tmtx_t ttype, dmtx_t dtype,
+                  const ValueType *mtx, const matrix_info x_info,
+                  ValueType *x) {
+    if (x_info.size[1] != 1) {
+        throw "Error!";
+    };
+    if (ttype == tmtx_t::lower) {
+        for (std::size_t row = 0; row < m_info.size[0]; ++row) {
+            ValueType local_res{0};
+            for (std::size_t col = 0; col < row; ++col) {
+                const std::size_t midx = row * m_info.stride + col;
+                local_res += mtx[midx] * x[col * x_info.stride];
+            }
+            const ValueType diag =
+                dtype == dmtx_t::unit ? 1 : mtx[row * m_info.stride + row];
+            const auto rhs_val = x[row * x_info.stride];
+            x[row * x_info.stride] = (rhs_val - local_res) / diag;
+        }
+    } else {
+        for (std::size_t inv_row = 0; inv_row < m_info.size[0]; ++inv_row) {
+            const auto row = m_info.size[0] - 1 - inv_row;
+            ValueType local_res{0};
+            for (std::size_t inv_col = 0; inv_col < inv_row; ++inv_col) {
+                const auto col = m_info.size[0] - 1 - inv_col;
+                const std::size_t midx = row * m_info.stride + col;
+                local_res += mtx[midx] * x[col * x_info.stride];
+            }
+            const ValueType diag =
+                dtype == dmtx_t::unit ? 1 : mtx[row * m_info.stride + row];
+            const auto rhs_val = x[row * x_info.stride];
+            x[row * x_info.stride] = (rhs_val - local_res) / diag;
+        }
+    }
+}
+
+template <typename ValueType>
+void trsv(const matrix_info m_info, tmtx_t ttype, dmtx_t dtype,
+          const ValueType *mtx, const matrix_info x_info, ValueType *x) {
+    constexpr std::int32_t diag_block_size{kernel::WARP_SIZE};
+    const dim3 block_solve(diag_block_size, diag_block_size, 1);
+    // const dim3 grid(ceildiv<std::int32_t>(m_info.size[0], block_size), 1, 1);
+    const dim3 grid_solve(1, 1, 1);
+    auto run_solve = [&](std::int64_t block_start) {
+        if (dtype == dmtx_t::unit) {
+            kernel::lower_trsv<diag_block_size, dmtx_t::unit>
+                <<<grid_solve, block_solve>>>(block_start, m_info, mtx, x_info,
+                                              x);
+        } else {
+            kernel::lower_trsv<diag_block_size, dmtx_t::non_unit>
+                <<<grid_solve, block_solve>>>(block_start, m_info, mtx, x_info,
+                                              x);
+        }
+    };
+    const std::int32_t block_size_apply = kernel::WARP_SIZE * kernel::WARP_SIZE;
+    const dim3 block_apply(kernel::WARP_SIZE * kernel::WARP_SIZE);
+    for (std::int64_t block_start = 0; block_start < m_info.size[0];
+         block_start += kernel::WARP_SIZE) {
+        run_solve(block_start);
+        const dim3 grid_apply(
+            ceildiv(static_cast<std::int64_t>(m_info.size[0]) - block_start, static_cast<std::int64_t>(kernel::WARP_SIZE)),
+            1, 1);
+        //*
+        if (block_start + kernel::WARP_SIZE < m_info.size[0]) {
+            kernel::lower_trsv_apply<block_size_apply, kernel::WARP_SIZE>
+                <<<grid_apply, block_apply>>>(block_start, m_info, mtx, x_info,
+                                              x);
+        }
+        //*/
+    }
+}
+
+template <typename ArType, typename StType>
+void acc_trsv(const matrix_info m_info, tmtx_t ttype, dmtx_t dtype,
+              const StType *mtx, const matrix_info x_info, StType *x) {
+    /*
+    constexpr std::int32_t block_size{512};
+    const dim3 block(block_size, 1, 1);
+    // const dim3 grid(ceildiv<std::int32_t>(m_info.size[0], block_size), 1, 1);
+    const dim3 grid(m_info.size[0], 1, 1);
+
+    // Accessor Setup
+    constexpr std::size_t dimensionality{2};
+    std::array<std::size_t, dimensionality - 1> m_stride{m_info.stride};
+    std::array<std::size_t, dimensionality - 1> x_stride{x_info.stride};
+    std::array<std::size_t, dimensionality - 1> res_stride{res_info.stride};
+
+    using accessor =
+        gko::acc::reduced_row_major<dimensionality, ArType, StType>;
+    using range = gko::acc::range<accessor>;
+    using c_range = gko::acc::range<typename accessor::const_accessor>;
+    auto m_acc = c_range(m_info.size, mtx, m_stride);
+    auto x_acc = c_range(x_info.size, x, x_stride);
+    auto res_acc = range(res_info.size, res, res_stride);
+
+    kernel::acc_trsv<block_size>
+        <<<grid, block>>>(alpha, m_acc, x_acc, beta, res_acc);
+    */
+}
+
+#define BIND_CUBLAS_TRSV(ValueType, CublasName)                                \
+    void cublas_trsv(cublasHandle_t handle, cublasFillMode_t uplo,             \
+                     cublasOperation_t trans, cublasDiagType_t dig, int n,     \
+                     const ValueType *A, int lda, ValueType *x, int incx) {    \
+        CUBLAS_CALL(CublasName(handle, uplo, trans, dig, n, A, lda, x, incx)); \
+    }
+BIND_CUBLAS_TRSV(double, cublasDtrsv)
+BIND_CUBLAS_TRSV(float, cublasStrsv)
+#undef BIND_CUBLAS_TRSV
+
+template <typename ValueType>
+void cublas_trsv(cublasHandle_t handle, tmtx_t ttype, dmtx_t dtype,
+                 const matrix_info m_info, const ValueType *mtx,
+                 const matrix_info x_info, ValueType *x) {
+    // Note: CUBLAS expects the matrices to be stored in column major,
+    //       so it needs to be transposed & upper, lower needs to be swapped
+    cublasFillMode_t uplo = ttype == tmtx_t::upper ? CUBLAS_FILL_MODE_LOWER
+                                                   : CUBLAS_FILL_MODE_UPPER;
+    cublasDiagType_t diag =
+        dtype == dmtx_t::unit ? CUBLAS_DIAG_UNIT : CUBLAS_DIAG_NON_UNIT;
+    cublas_trsv(
+        handle, uplo, CUBLAS_OP_T, diag, static_cast<int>(m_info.size[0]), mtx,
+        static_cast<int>(m_info.stride), x, static_cast<int>(x_info.stride));
+}
+
